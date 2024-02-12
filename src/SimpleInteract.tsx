dd4816239c687076b7d251450094e6aa6bece2b5
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-02-11 23:53:25 -0500

    Update conversation refresh interval to 3 seconds instead of 30 seconds.

--- src/SimpleInteract.tsx
+++ src/SimpleInteract.tsx
@@ -57,7 +57,7 @@ export const SimpleInteract: React.FC<IInteract> = ({ dappAddress, setDappAddres
     refreshConversations();
     const interval = setInterval(() => {
       refreshConversations();
-    }, 30000); // Refresh every 30 seconds
+    }, 3000); // Refresh every 3 seconds
 
     return () => clearInterval(interval);
   }, [contractAddress]);
